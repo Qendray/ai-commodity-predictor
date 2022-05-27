32f077cd43ece6286ff5f72dc2dd8d89d3189964
Qendray <106153839+Qendray@users.noreply.github.com>
2022-05-27 05:19:16 -0700

    Add files via upload

--- main1.py
+++ main1.py
@@ -2,10 +2,10 @@ from tracemalloc import start
 import streamlit as st
 from datetime import date
  
- import yahoofinance as yf
- from facebookprophet import Prophet
- from facebookprophet.plot import plot_plotly
- from plotly import graph_objs as go
+import yfinance as yf
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
 
 START = "2016-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
