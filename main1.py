cfe4aed17d810919a9b9600d5a73c7a46a8a4ce4
Qendray <106153839+Qendray@users.noreply.github.com>
2022-05-29 02:38:02 -0700

    Add files via upload

--- main1.py
+++ main1.py
@@ -35,7 +35,7 @@ def plot_raw_data():
     fig = go.Figure()
     fig.add_trace(go.scatter(x=data['Date'], y=data['Open'], name='commodities_open'))
     fig.add_trace(go.scatter(x=data['Date'], y=data['Close'], name='commodities_close'))
-    fig.layout.update(title_text"Time Series Data", xaxis_rangeslider_visible=True)
+    fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
     st.plotly_chart(fig)
 
 plot_raw_data()
