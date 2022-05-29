65d070c58a88a94f0860210bcc807305e6b1c1a7
Qendray <106153839+Qendray@users.noreply.github.com>
2022-05-29 11:59:29 -0700

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from datetime import date
  
-from yahoofinance import yfinance
 import yfinance as yf
+from yahoofinance import yfinance
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
